6c2081deb09bf6c42fdbf3608ab979c8a425361b
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-11-20 10:16:59 -0300

    Aula 02

---
+++ arrayTest.js
@@ -0,0 +1,35 @@
+const posts = [
+    {
+        id: 1,
+        description: "Uma foto",
+        image: "http://placecats.com/millie/300/150",
+    },
+    {
+        id: 2,
+        description: "Um pôr do sol incrível",
+        image: "http://placecats.com/millie/300/150",
+    },
+    {
+        id: 3,
+        description: "Um gatinho curioso",
+        image: "http://placecats.com/millie/300/150",
+    },
+    {
+        id: 4,
+        description: "Paisagem de montanha",
+        image: "http://placecats.com/millie/300/150",
+    },
+    {
+        id: 5,
+        description: "Uma cidade à noite",
+        image: "http://placecats.com/millie/300/150",
+    },
+    {
+        id: 6,
+        description: "Uma praia tranquila",
+        image: "http://placecats.com/millie/300/150",
+    }
+];
+
+
+export default posts;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT || 3333 ; 
+import posts  from './arrayTest.js';
+
+const PORT = process.env.PORT || 3333; 
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,21 @@ app.get('/', (request, response) => {
     response.status(200).json({message: "OK"});
 });
 
+app.get('/posts', (request, response) => {
+    response.status(200).json({'message': posts});
+});
+
+const buscar = (id) => {
+    let post = posts.find((post) => post.id === Number(id));
+    if (!post) return undefined;
+    const { description, image } = post;
+    return { description, image };
+};
+
+app.get('/posts/:id', (request, response) => {
+    response.status(200).json(buscar(request.params.id));
+});
+
 app.listen(PORT, () => {
-    console.log(`Rodando na porta ${PORT}`);    
+    console.log(`Rodando na porta ${PORT}`);
 });
